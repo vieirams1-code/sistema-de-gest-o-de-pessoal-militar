d82c804d39a5f4bfe410216336e6ec3b154d610d
Edson Vieira de Souza <vieirams1@gmail.com>
2026-03-06 18:53:02 +0000

    File changes

--- src/pages/CadastrarMilitar.jsx
+++ src/pages/CadastrarMilitar.jsx
@@ -70,7 +70,8 @@ const initialFormData = {
   cidade: '',
   uf: '',
   complemento: '',
-  habilidades: []
+  habilidades: [],
+  link_alteracoes_anteriores: ''
 };
 
 const UFS = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
