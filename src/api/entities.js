6cb5378acd7c8853c4845ccc9be0a158c3940686
vieirams1-code <vieirams1@gmail.com>
2026-03-25 17:55:39 -0400

    Add PunicaoDisciplinar base44 entity mapping

--- src/api/entities.js
+++ src/api/entities.js
@@ -3,6 +3,7 @@ import { base44 } from './base44Client';
 
 export const Query = base44.entities.Query;
 export const PublicacaoCompilada = base44.entities.PublicacaoCompilada;
+export const PunicaoDisciplinar = base44.entities.PunicaoDisciplinar;
 
 
 
