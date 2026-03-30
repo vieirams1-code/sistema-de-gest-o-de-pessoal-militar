d796ddb2311244a443fa2551f1f6eb385e2680c7
vieirams1-code <vieirams1@gmail.com>
2026-03-30 17:38:31 -0400

    feat: preparar entidade de histórico de promoção militar

--- src/api/entities.js
+++ src/api/entities.js
@@ -3,6 +3,7 @@ import { base44 } from './base44Client';
 export const Query = base44.entities.Query;
 
 export const PunicaoDisciplinar = base44.entities.PunicaoDisciplinar;
+export const HistoricoPromocaoMilitar = base44.entities.HistoricoPromocaoMilitar;
 
 // auth sdk:
 export const User = base44.auth;
