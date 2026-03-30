67e26d54b01aa60489ea5d2e7eb3c431c57441d5
vieirams1-code <vieirams1@gmail.com>
2026-03-30 12:20:04 -0400

    Corrige base de tarefas operacionais e destinação por escopo

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,11 +1,26 @@
 import { base44 } from './base44Client';
 
+function resolveEntity(aliases) {
+  for (const alias of aliases) {
+    if (base44.entities?.[alias]) return base44.entities[alias];
+  }
+  return undefined;
+}
+
 export const Query = base44.entities.Query;
 
 export const PunicaoDisciplinar = base44.entities.PunicaoDisciplinar;
-export const TarefaOperacional = base44.entities.TarefaOperacional;
-export const TarefaOperacionalDestinatario = base44.entities.TarefaOperacionalDestinatario;
-export const TarefaOperacionalHistorico = base44.entities.TarefaOperacionalHistorico;
+export const TarefaOperacional = resolveEntity(['TarefaOperacional', 'TarefasOperacionais', 'tarefaOperacional']);
+export const TarefaOperacionalDestinatario = resolveEntity([
+  'TarefaOperacionalDestinatario',
+  'TarefaOperacionalDestinatarios',
+  'tarefaOperacionalDestinatario',
+]);
+export const TarefaOperacionalHistorico = resolveEntity([
+  'TarefaOperacionalHistorico',
+  'TarefaOperacionalHistoricos',
+  'tarefaOperacionalHistorico',
+]);
 
 // auth sdk:
 export const User = base44.auth;
