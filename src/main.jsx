a094fbed8ede48694293f37fa8ef5a5aa59a4e7f
vieirams1-code <vieirams1@gmail.com>
2026-04-15 15:54:07 -0400

    Remove módulo de Migração de Atestados

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import { ImportacaoMilitares, ImportacaoAtestados, ImportacaoAlteracoesLegado, PunicaoDisciplinar } from '@/api/entities'
+import { ImportacaoMilitares, ImportacaoAlteracoesLegado, PunicaoDisciplinar } from '@/api/entities'
 import { base44 } from '@/api/base44Client'
 
 
 // Mantém o módulo de entidades carregado no bundle para sincronização/publicação no runtime Base44.
 void ImportacaoMilitares;
-void ImportacaoAtestados;
 void ImportacaoAlteracoesLegado;
 void PunicaoDisciplinar;
 void base44.entities.ImportacaoMilitares;
-void base44.entities.ImportacaoAtestados;
 void base44.entities.ImportacaoAlteracoesLegado;
 void base44.entities.PunicaoDisciplinar;
 
@@ -30,4 +28,3 @@ if (import.meta.hot) {
     window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
   });
 }
-
