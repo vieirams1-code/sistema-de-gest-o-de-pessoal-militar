ea9d9719b5e3ca73833ce6c0f923a8bd6c0226e6
vieirams1-code <vieirams1@gmail.com>
2026-05-27 00:18:21 -0400

    feat(utils): add shared scope and query key factories

---
+++ src/utils/queryKeys.js
@@ -0,0 +1,16 @@
+export const queryKeys = {
+  lotacoes: {
+    root: () => ['lotacoes'],
+  },
+  funcoes: {
+    root: () => ['funcoes'],
+  },
+  cards: {
+    root: () => ['cards'],
+  },
+  ferias: {
+    edicao: () => ['ferias', 'edicao'],
+  },
+};
+
+export default queryKeys;
