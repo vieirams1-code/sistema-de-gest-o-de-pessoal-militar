0c83c52daea1f3abd4fab1234404dea0bb3e33bd
vieirams1-code <vieirams1@gmail.com>
2026-08-25 13:42:39 -0400

    fix(pages): add missing Portal page components to git

---
+++ src/pages/Portal.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PortalApp from '@/portal/PortalApp';
+
+export default function Portal() {
+  return <PortalApp />;
+}
---
+++ src/pages/PortalCadastro.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PortalApp from '@/portal/PortalApp';
+
+export default function PortalCadastro() {
+  return <PortalApp initialView="CADASTRO" />;
+}
